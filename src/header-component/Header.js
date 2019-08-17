f9d45060d1d8a7198f69dbf1fd889a0e7f4d4aec
Bryan Hernandez A <hernandezbryan2893@gmail.com>
2019-08-17 15:10:07 -0600

    initial commit

---
+++ src/header-component/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = function(props){
+    return (
+        <header className="Header-topnav">
+            
+        </header>
+    );
+};
+
+export default Header;
