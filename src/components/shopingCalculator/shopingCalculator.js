aca79f9bdd3392dc27735a320ef84828719de81b
Bryan Hernandez A <hernandezbryan2893@gmail.com>
2019-08-31 23:02:24 -0600

    Calculator component works fine with keyboard inputs and button inputs

--- src/components/shopingCalculator/shopingCalculator.js
+++ src/components/shopingCalculator/shopingCalculator.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './shopingCalculator.css';
 import CloseButton from './closeButton/CloseButton';
 
@@ -8,41 +8,80 @@ const mapStateToProps = state => {
     return { calculator: state.salesWindowState.calculator }
 };
 
-const ShopingCalculatorConnected = function ({ calculator }) {
-    let isOpen = "hidden";
-    if (calculator.isOpen) {
-        isOpen = "visible";
+
+class ShopingCalculatorConnected extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { txtQuantity: '' };
+        this.handleNumberButton = this.handleNumberButton.bind(this);
+        this.handleOnChangeTxtQuantity = this.handleOnChangeTxtQuantity.bind(this);
     }
-    return (
-        <div className="shopingCalculator-container" style={{ visibility: isOpen }}>
-            <div className="shopingCalculator-imgContainer">
-                <img className="shopingCalculator-image" alt="img" src={calculator.foodObject.image} />
-                <CloseButton />
+
+    handleOnChangeTxtQuantity(event) {
+        let number = parseInt(event.target.value);
+        if (number || number === 0) {
+            if (this.state.txtQuantity === '' && number === 0) {
+                this.setState({ txtQuantity: '' });
+            }
+            else {
+                this.setState({ txtQuantity: String(number) });
+            }
+        } else {
+            this.setState({ txtQuantity: '' });
+        }
+    }
+
+    handleNumberButton(event) {
+        let number = parseInt(event.target.value);
+        if (number || number === 0) {
+            if (this.state.txtQuantity === '' && number === 0) {
+                this.setState({ txtQuantity: '' });
+            } else {
+                this.setState(previosState => ({
+                    txtQuantity: previosState.txtQuantity + String(number)
+                }));
+            }
+        } else {
+            this.setState({ txtQuantity: '' });
+        }
+    }
+
+    render() {
+        let isOpen = "hidden";
+        if (this.props.calculator.isOpen) {
+            isOpen = "visible";
+        }
+        return (
+            <div className="shopingCalculator-container" style={{ visibility: isOpen }}>
+                <div className="shopingCalculator-imgContainer">
+                    <img className="shopingCalculator-image" alt="img" src={this.props.calculator.foodObject.image} />
+                    <CloseButton />
+                </div>
+                <div className="shopingCalculator-calculatorButtonsContainer">
+                    <input type="text" placeholder="cantidad..." value={this.state.txtQuantity} onChange={this.handleOnChangeTxtQuantity} />
+                    <div className="shopingCalculator-buttonRow">
+                        <button value="1" onClick={this.handleNumberButton}>1</button>
+                        <button value="2" onClick={this.handleNumberButton}>2</button>
+                        <button value="3" onClick={this.handleNumberButton}>3</button>
+                    </div>
+                    <div className="shopingCalculator-buttonRow">
+                        <button value="4" onClick={this.handleNumberButton}>4</button>
+                        <button value="5" onClick={this.handleNumberButton}>5</button>
+                        <button value="6" onClick={this.handleNumberButton}>6</button>
+                    </div>
+                    <div className="shopingCalculator-buttonRow">
+                        <button value="7" onClick={this.handleNumberButton}>7</button>
+                        <button value="8" onClick={this.handleNumberButton}>8</button>
+                        <button value="9" onClick={this.handleNumberButton}>9</button>
+                    </div>
+                    <div className="shopingCalculator-buttonRow">
+                        <button value="0" onClick={this.handleNumberButton}>0</button>
+                    </div>
+                </div>
             </div>
-            <div className="shopingCalculator-calculatorButtonsContainer">
-                <input type="text" placeholder="cantidad..." />
-                <div className="shopingCalculator-buttonRow">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                </div>
-                <div className="shopingCalculator-buttonRow">
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                </div>
-                <div className="shopingCalculator-buttonRow">
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
-                </div>
-                <div className="shopingCalculator-buttonRow">
-                    <button>0</button>
-                </div>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+}
 
 const ShopingCalculator = connect(mapStateToProps)(ShopingCalculatorConnected);
 
