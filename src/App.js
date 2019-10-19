a6fa1e0f39b36b3f9c76feb66503319ba28bbd1d
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-10-19 15:06:48 -0600

    Provider was moved to App component.

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ import Header from './components/Header/Header';
 import SalesWindow from './components/SalesWindow/SalesWindow';
 import Alert from './components/Alert/Alert';
 
+import {Provider} from 'react-redux';
+import store from './redux/store';
+
 function App() {
   return (
-    <div className="App">
-      <Alert/>
-      <Header/>
-      <SalesWindow/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Alert/>
+        <Header/>
+        <SalesWindow/>
+      </div>
+    </Provider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,16 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import {Provider} from 'react-redux';
-import store from './redux/store';
-import {BrowserRouter as Router} from 'react-router-dom';
-
-ReactDOM.render(<Provider store={store}>
-                    <Router>
-                        <App />
-                    </Router>
-                </Provider>, 
-    document.getElementById('root')
+ReactDOM.render(<App />, document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
