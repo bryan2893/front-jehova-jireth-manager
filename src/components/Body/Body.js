9140759fb370c9b5518db15fbdf124226ad3b370
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-10-19 18:06:23 -0600

    add Link tags to header and Body Component was created.

---
+++ src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+import logo from '../../test-images/logo.png';
+
+import {Switch,Route} from 'react-router-dom';
+
+const Body = () => (
+        <header className="Header-topnav">
+            
+        </header>
+    );
+
+export default Body;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Header.css';
 import logo from '../../test-images/logo.png';
+import {Link} from 'react-router-dom';
 
 const Header = function(props){
     return (
         <header className="Header-topnav">
             <img className="Header-logo" alt="img" src={logo} />
             <span className="Header-brandName">Jehová Jireth</span>
+            <Link className="Header-link" to="/salesWindow">Ventana de ventas</Link>
+            <Link className="Header-link" to="/cierres">Cierres</Link>
         </header>
     );
 };
