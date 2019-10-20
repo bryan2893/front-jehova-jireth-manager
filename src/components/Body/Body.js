098c9f3d73a04c874c697931ad58c9402ec2ba2b
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-10-19 19:41:09 -0600

    React router was set succesfully.

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,13 +1,22 @@
 import React from 'react';
-import './Header.css';
-import logo from '../../test-images/logo.png';
-
 import {Switch,Route} from 'react-router-dom';
+import './Body.css';
+
+import SalesWindow from '../SalesWindow/SalesWindow';
+
+const Home = function(props){
+    return (
+        <h1>HOME</h1>
+    );
+}
 
 const Body = () => (
-        <header className="Header-topnav">
-            
-        </header>
+        <div className="Body-container">
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/sales" component={SalesWindow} />
+            </Switch>
+        </div>
     );
 
 export default Body;
