5c7058cd2eabe9fc1062804f62d30cc5ea7e8467
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-10-23 18:21:19 -0600

    Redirect was added to Login component to redirect to Home Component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './App.css';
-//import SalesWindow from './components/SalesWindow/SalesWindow';
 import Alert from './components/Alert/Alert';
 import Home from './components/Home/Home';
 
 import {Provider} from 'react-redux';
 import store from './redux/store';
 
-//import Login from './components/Login/Login';
+import {Switch,Route} from 'react-router-dom';
+
+import Login from './components/Login/Login';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
         <Alert />
-        <Home />
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/app" component={Home} />
+        </Switch>
       </div>
     </Provider>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './Login.css';
 import logo from '../../test-images/logo.png';
 
+import {Redirect} from 'react-router-dom';
+
 export default class Login extends React.Component{
     constructor(props){
         super(props);
         this.handleUsername = this.handleUsername.bind(this);
         this.handlePassword = this.handlePassword.bind(this);
         this.handleOnSubmit = this.handleOnSubmit.bind(this);
-        this.state = {usernameText:"",passwordText:""};
+        this.state = {usernameText:"",passwordText:"",validUser:false};
     }
 
     handleUsername(event){
@@ -21,18 +23,23 @@ export default class Login extends React.Component{
 
     handleOnSubmit(event){
         event.preventDefault();
+        this.setState({validUser:true});
     }
 
     render(){
-        return (
-            <div className="Login-window-container">
-                <form onSubmit={this.handleOnSubmit} className="Login-form ">
-                    <span className="Login-imgContainer"><img className="Login-img" src={logo}/></span>
-                    <input type="text" placeholder="username" onChange={this.handleUsername}/>
-                    <input type="password" placeholder="password" onChange={this.handlePassword}/>
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        );
+        if (this.state.validUser){
+            return <Redirect to="/app" />;
+        }else{
+            return (
+                <div className="Login-window-container">
+                    <form onSubmit={this.handleOnSubmit} className="Login-form ">
+                        <span className="Login-imgContainer"><img className="Login-img" src={logo}/></span>
+                        <input type="text" placeholder="username" onChange={this.handleUsername}/>
+                        <input type="password" placeholder="password" onChange={this.handlePassword}/>
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            );
+        }
     }
 }
