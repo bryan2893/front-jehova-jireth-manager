ff75ae6dd5d3831c89191b00458a4168dab9be24
Bryan Hernandez <hernandezbryan2893gmail.com>
2019-10-23 19:33:56 -0600

    Some little changes, no important.

--- src/components/Home/Body/Body.js
+++ src/components/Home/Body/Body.js
@@ -4,12 +4,6 @@ import './Body.css';
 
 import SalesWindow from '../SalesWindow/SalesWindow';
 
-const Algo = function(props){
-    return (
-        <h1>HOME</h1>
-    );
-}
-
 const FacturasWindow = function(props){
     return (
         <h1>FACTURAS</h1>
@@ -19,9 +13,8 @@ const FacturasWindow = function(props){
 const Body = () => (
         <div className="Body-container">
             <Switch>
-                <Route exact path="/" component={Algo} />
-                <Route path="/sales" component={SalesWindow} />
-                <Route path="/facturas" component={FacturasWindow} />
+                <Route path="/app/sales" component={SalesWindow} />
+                <Route path="/app/facturas" component={FacturasWindow} />
             </Switch>
         </div>
     );
