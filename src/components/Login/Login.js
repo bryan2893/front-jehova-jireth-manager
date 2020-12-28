74cdf7de385b6f878d6f6913235f32c13505d782
Bryan Hernandez <hernandezbryan2893gmail.com>
2020-12-28 14:07:49 -0600

    Working with materilize
    material-ui/core was uninstall from project, materialize is using instead. A hooks directory was created and Login component was redifined from a class to a function component.

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from 'materialize-css';
 import './Login.css';
 import logo from '../../test-images/logo.png';
 
@@ -6,60 +8,37 @@ import {Redirect} from 'react-router-dom';
 
 import {login} from '../../service/auth';
 
-export default class Login extends React.Component{
-    constructor(props){
-        super(props);
-        this.handleUsername = this.handleUsername.bind(this);
-        this.handlePassword = this.handlePassword.bind(this);
-        this.handleOnSubmit = this.handleOnSubmit.bind(this);
-        this.state = {usernameText:"",passwordText:"",validUser:false};
-        this.idInput = React.createRef();
-    }
 
-    componentDidMount(){
-        this.idInput.current.focus();
-    }
+function Login(props){
 
-    handleUsername(event){
-        this.setState({usernameText:event.target.value});
-    }
-
-    handlePassword(event){
-        this.setState({passwordText:event.target.value});
-    }
-
-    handleOnSubmit(event){
+    function handleOnSubmit(event){
         event.preventDefault();
-        if (this.state.usernameText === "" || this.state.passwordText === ""){
-            console.log("Datos insuficientes!");
-            return;
-        }else{
-            login(this.state.usernameText,this.state.passwordText).then((result) => {
-                if(result.status === 200){
-                    let token = result.data.token;
-                    console.log("Token: "+token);
-                    this.setState({validUser:true});
-                }
-            }).catch((error) => {
-                console.log(error.message);
-            });
-        }
+        let [cedula,contraseña] = event.target;
+        // cedula.value and contraseña.value get values of each one...
     }
 
-    render(){
-        if (this.state.validUser){
-            return <Redirect to="/app/sales" />;
-        }else{
-            return (
-                <div className="Login-window-container">
-                    <form onSubmit={this.handleOnSubmit} className="Login-form ">
-                        <span className="Login-imgContainer"><img className="Login-img" src={logo} alt="logo jehovaJireth"/></span>
-                        <input ref={this.idInput} type="text" placeholder="cedula" onChange={this.handleUsername}/>
-                        <input type="password" placeholder="contraseña" onChange={this.handlePassword}/>
-                        <button type="submit">Login</button>
-                    </form>
+    return (
+        <div className="Login-window-container">
+            <form onSubmit={handleOnSubmit} className="Login-form ">
+                <span className="Login-imgContainer"><img className="Login-img" src={logo} alt="logo jehovaJireth"/></span>
+
+                <div id="inpCedula" className="input-field Login-input">
+                    <input id="cedula" type="text" className="validate" name="cedula"/>
+                    <label htmlFor="cedula">Cedula</label>
                 </div>
-            );
-        }
-    }
+
+                <div id="inpContraseña" className="input-field Login-input">
+                    <input id="contraseña" type="password" className="validate" name="contraseña"/>
+                    <label htmlFor="contraseña">Contraseña</label>
+                </div>
+
+                <button className="btn waves-effect waves-light" type="submit">
+                    Ingresar
+                </button>
+
+            </form>
+        </div>
+    )
 }
+
+export default Login;
